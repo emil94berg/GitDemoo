a355409f210e3acdf330eeb68c062992bb6f2d2e
Emil Berg <emil94berg@gmail.com>
2024-10-28 10:22:29 +0100

    Add loop method

---
+++ GitDemo/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class Helpers
+    {
+        public static void MyLoop()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
